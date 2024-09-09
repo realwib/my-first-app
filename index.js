861af348fcc6d620e0e185429988812896017ea0
realwib <williamisaac7@gmail.com>
2024-09-09 02:22:38 -0400

    Serve HTML content from the server

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading index.html');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Page not found');
+  }
 });
 
 server.listen(port, hostname, () => {
